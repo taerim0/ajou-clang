f73986d4e2151244d5b5bfd124f4322ef1b2dbeb
taerim0 <taerim@ajou.ac.kr>
2024-05-17 20:58:26 +0900

    ADD : mysql connect

--- back/index.js
+++ back/index.js
@@ -1,18 +1,20 @@
-// express 불러오기
 const express = require('express');
-// express 인스턴스 생성
+const db = require('./lib/db.js');
 const app = express();
-// 포트 정보
-const port = 3000;
+const port = 1542;
 
-// 라우트 설정
-// HTTP GET 방식으로 '/' 경로를 요청하였을 때
-// Hello World!라는 문자열을 결과값으로 보냄
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  db.query('SELECT * FROM user_logindata WHERE id = 1', (error, data, fields) => {
+
+    if (error) throw error;
+
+    console.log('connected with DB');
+    console.log('test query : ', data);
+
+    res.send(data);
+  });
 });
 
-// 서버 실행
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`api server started on localhost:${port}`);
 });
